fa61ec4a639d7845439fd4e1ff541eb759e7f2cd
tahai <tahai@10.40.0.84>
2020-09-22 21:13:48 -0700

    perf

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 public class SortingAlgorithms {
 	public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int[] actual = { 5, 1, 6, 2, 3, 4 };
+	    int[] expected = { 1, 2, 3, 4, 5, 6 };
+	    int[] result = mergeSort(actual, actual.length);
+	    System.out.println(Arrays.equals(expected, result)) ;
 	}
 
 	public Object[] bubbleSort() {
@@ -13,7 +19,43 @@ public class SortingAlgorithms {
 		return null;
 	}
 
-	public Object[] mergeSort() {
-		return null;
+	public static int[] mergeSort(int[] toSort, int n) {
+	    if (n < 2) {
+	        return toSort;
+	    }
+	    int mid = n / 2;
+	    int[] a = new int[mid];
+	    int[] b = new int[n - mid];
+	 
+	    for (int i = 0; i < mid; i++) {
+	        a[i] = toSort[i];
+	    }
+	    for (int i = mid; i < n; i++) {
+	        b[i - mid] = toSort[i];
+	    }
+	    mergeSort(a, mid);
+	    mergeSort(b, n - mid);
+	 
+	    merge(toSort, a, b, mid, n - mid);
+	    return toSort ;
+	}
+	
+	public static void merge (int[] a, int[] l, int[] r, int left, int right) {
+	 
+	    int i = 0, j = 0, k = 0;
+	    while (i < left && j < right) {
+	        if (l[i] <= r[j]) {
+	            a[k++] = l[i++];
+	        }
+	        else {
+	            a[k++] = r[j++];
+	        }
+	    }
+	    while (i < left) {
+	        a[k++] = l[i++];
+	    }
+	    while (j < right) {
+	        a[k++] = r[j++];
+	    }
 	}
 }
