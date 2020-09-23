c66e0f634fc39fdc7e3619f69bc7f9a7b5258204
Andrew Johnston <andrewj@sd48.net>
2020-09-22 21:41:47 -0700

    randomly generated test array

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,6 +1,23 @@
+import java.util.Random;
+
 public class SortingAlgorithms {
 	public static void main(String[] args) {
-        System.out.println("Hello, World!");
+		Random random = new Random();
+
+		Integer[] arr = new Integer[10000];
+
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = random.nextInt(10000);
+
+		long bubbleSortStartTime = System.currentTimeMillis();
+
+		long bubbleSortEndTime = System.currentTimeMillis();
+
+		long insertionSortStartTime = System.currentTimeMillis();
+		long insertionSortEndTime = System.currentTimeMillis();
+
+		long mergeSortStartTime = System.currentTimeMillis();
+		long mergeSortEndTime = System.currentTimeMillis();
 	}
 
 	public Object[] bubbleSort() {
