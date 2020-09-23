70ea55b624ae524fe63728b8fdefb705e3bb215c
Andrew Johnston <andrewj@sd48.net>
2020-09-22 23:27:02 -0700

    arrays changed to int

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -69,44 +69,4 @@ public class SortingAlgorithms {
 		}
 		return r;
 	}
-
-//	public static int[] mergeSort(int[] toSort, int n) {
-//	    if (n < 2) {
-//	        return toSort;
-//	    }
-//	    int mid = n / 2;
-//	    int[] a = new int[mid];
-//	    int[] b = new int[n - mid];
-//
-//	    for (int i = 0; i < mid; i++) {
-//	        a[i] = toSort[i];
-//	    }
-//	    for (int i = mid; i < n; i++) {
-//	        b[i - mid] = toSort[i];
-//	    }
-//	    mergeSort(a, mid);
-//	    mergeSort(b, n - mid);
-//
-//	    merge(toSort, a, b, mid, n - mid);
-//	    return toSort ;
-//	}
-//
-//	public static void merge (int[] a, int[] l, int[] r, int left, int right) {
-//
-//	    int i = 0, j = 0, k = 0;
-//	    while (i < left && j < right) {
-//	        if (l[i] <= r[j]) {
-//	            a[k++] = l[i++];
-//	        }
-//	        else {
-//	            a[k++] = r[j++];
-//	        }
-//	    }
-//	    while (i < left) {
-//	        a[k++] = l[i++];
-//	    }
-//	    while (j < right) {
-//	        a[k++] = r[j++];
-//	    }
-//	}
 }
