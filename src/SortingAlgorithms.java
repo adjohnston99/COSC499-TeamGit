7bf05fc0743c9a2133348453b6cd043d38aa4cf8
Andrew Johnston <andrewj@sd48.net>
2020-09-23 00:38:09 -0700

    merge sort replaced with selection sort oin master cause it's late and I'm tired

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -12,22 +12,23 @@ public class SortingAlgorithms {
 		for (int i = 0; i < arr.length; i++)
 			arr[i] = random.nextInt(size);
 
+		System.out.println("Test array of size " + size + ": " + Arrays.toString(arr));
+
 		long bubbleSortStartTime = System.currentTimeMillis();
-		int[] bubbleSorted = bubbleSort(arr);
+//		int[] bubbleSorted = bubbleSort(arr);
 		long bubbleSortEndTime = System.currentTimeMillis();
 
 		long insertionSortStartTime = System.currentTimeMillis();
-		int[] insertionSorted = insertionSort(arr);
+//		int[] insertionSorted = insertionSort(arr);
 		long insertionSortEndTime = System.currentTimeMillis();
 
-		long mergeSortStartTime = System.currentTimeMillis();
-		int[] mergeSorted = mergeSort(arr);
-		long mergeSortEndTime = System.currentTimeMillis();
+		long selectionSortStartTime = System.currentTimeMillis();
+		int[] selectionSorted = selectionSort(arr);
+		long selectionSortEndTime = System.currentTimeMillis();
 
-		System.out.println("Test array of size " + size + ": " + Arrays.toString(arr));
 		System.out.println("Bubble sort took " + (bubbleSortEndTime - bubbleSortStartTime) + "ms");
 		System.out.println("Insertion sort took " + (insertionSortEndTime - insertionSortStartTime) + "ms");
-		System.out.println("Merge sort took " + (mergeSortEndTime - mergeSortStartTime) + "ms");
+		System.out.println("Merge sort took " + (selectionSortEndTime - selectionSortStartTime) + "ms");
 	}
 
 	public static int[] bubbleSort(int[] array) {
@@ -59,32 +60,55 @@ public class SortingAlgorithms {
 		return a;
 	}
 
-	public static int[] mergeSort(int[] a) {
-		if (a.length <= 1)
-			return a;
-		else {
-			int[] f = new int[(a.length+1)/2];
-			int[] b = new int[a.length - f.length];
-			return mergeSort(f, b);
-		}
-	}
-
-	public static int[] mergeSort(int[] a, int[] b) {
-		return merge(mergeSort(a), mergeSort(b));
-	}
-
-	private static int[] merge(int[] a, int[] b) {
-		int[] r = new int[a.length + b.length];
-		int j = 0, k = 0;
+	public static int[] selectionSort(int[] a) {
+		int[] r = new int[a.length];
 		for (int i = 0; i < r.length; i++) {
-			if (a[j] < b[k]) {
-				r[i] = a[j];
-				j++;
-			} else {
-				r[i] = b[k];
-				k++;
+			int min = a[i];
+			for (int j = i; j < a.length; j++) {
+				min = (a[j] < min)? a[j] : min;
 			}
+			r[i] = min;
 		}
 		return r;
 	}
+
+//	public static int[] mergeSort(int[] a) {
+////		System.out.println(Arrays.toString(a));
+//		if (a.length <= 1) {
+//			return a;
+//		} else {
+//			int[] f = new int[(a.length+1)/2];
+//			int[] b = new int[a.length - f.length];
+//
+//			System.arraycopy(a, 0, f, 0, f.length);
+//			System.arraycopy(a, f.length, b, 0, b.length);
+//
+////			System.out.println(Arrays.toString(f) + ", " + Arrays.toString(b));
+//			return merge(mergeSort(f), mergeSort(b));
+//		}
+//	}
+//
+//	private static int[] merge(int[] a, int[] b) {
+//		System.out.println(Arrays.toString(a) + ", " + Arrays.toString(b));
+//		int[] r = new int[a.length + b.length];
+//		int j = 0, k = 0;
+//
+//		for (int i = 0; i < r.length; i++) {
+//			System.out.println("a[j]: " + a[j] + ", b[k]: " + b[k]);
+//			System.out.println(a.length + ", " + b.length);
+//			System.out.println("i: " + i + ", j: " + j + ", k: " + k);
+//			if (j < a.length && k < b.length) {
+//				if (a[j] < b[k]) {
+//					r[i] = a[j++];
+//				} else {
+//					r[i] = b[k++];
+//				}
+//			} else if (j >= a.length) {
+//				r[i] = b[k++];
+//			} else if (k >= b.length) {
+//				r[i] = a[j++];
+//			}
+//		}
+//		return r;
+//	}
 }
